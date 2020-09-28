2b0824caa5ef7132ded2d6035abe8154d3c96350
KangJunewoo <brianjune.dev@gmail.com>
2020-09-28 11:31:28 +0900

    [FIX] urls.py 앞에 / 붙여서 warning떴던거 해결

--- learningfair/applications/urls.py
+++ learningfair/applications/urls.py
@@ -5,8 +5,8 @@ app_name = 'applications'
 
 urlpatterns = [
     path('hotprojects', views.hotprojects, name='hotprojects'),
-    path('/classes/<int:class_id>/projects', views.classes, name='class_projects'),
-    path('/projects', views.projects, name='projects'),
-    path('/notices', views.notices, name='notices'),
-    path('/projects/<int:project_id>/likes', views.project_likes, name='edit_likes')
+    path('classes/<class_id>/projects', views.classes, name='class_projects'),
+    path('projects', views.projects, name='projects'),
+    path('notices', views.notices, name='notices'),
+    path('projects/<project_id>/likes', views.project_likes, name='edit_likes')
 ]
