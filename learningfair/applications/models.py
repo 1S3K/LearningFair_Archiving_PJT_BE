d1efc7b7c733f91ca56f2c2a7eee88db45d16860
Heedong Kim <ruthetum@gmail.com>
2020-09-28 10:40:40 +0900

    [FEAT] 모델 작성 및 migration

--- learningfair/applications/models.py
+++ learningfair/applications/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class User(models.Model):
+    studentId = models.CharField(max_length=10, null=False) # 학번
+    name = models.CharField(max_length=15, null=False) # 이름
+
+class Project(models.Model):
+    title = models.CharField(max_length=30, null=False) # 프로젝트 제목
+    campus = models.IntegerField(null=False) # 캠퍼스
+    lecture = models.IntegerField(null=False) # 분반
+    group = models.IntegerField(null=False) # 분반 내 각 팀
+    groupName = models.CharField(max_length=30, null=False)
+    members = models.CharField(max_length=100, null=False) # 프로젝트 멤버
+    description = models.TextField(null=False)  # 프로젝트 설명
+    pdf = models.TextField()    # PDF 파일 경로
+    video = models.TextField()  # Video 파일 경로
+    likeCount = models.IntegerField(default=0)  # 좋아요 수
+
+class Like(models.Model):
+    userId = models.TextField(null=False)   # 유저 고유 ID
+    projectId = models.TextField(null=False)    # 프로젝트 고유 ID
+
+class Notice(models.Model):
+    title = models.CharField(max_length=50) # 제목
+    contents = models.TextField(null=False) # 내용
+    date = models.DateTimeField(default=timezone.now, null=False) # 작성일
+
