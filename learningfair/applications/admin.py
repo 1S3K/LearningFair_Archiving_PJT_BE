0a85485962f7f43bfed62d37dd2f987e85c91ee1
Heedong Kim <ruthetum@gmail.com>
2020-09-28 11:16:43 +0900

    [fEAT] Admin 커스터마이징

--- learningfair/applications/admin.py
+++ learningfair/applications/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import User, Project, Like, Notice
 
-# Register your models here.
+# admin customizing
+@admin.register(User) # decorator 형태로 등록
+class StoryAdmin(admin.ModelAdmin):
+	list_display = ['studentId', 'name']
+
+@admin.register(Project)
+class StoryAdmin(admin.ModelAdmin):
+	list_display = ['title', 'lecture', 'group', 'groupName', 'members', 'description', 'pdf', 'video', 'likeCount']
+
+@admin.register(Like)
+class StoryAdmin(admin.ModelAdmin):
+	list_display = ['userId', 'projectId']
+
+@admin.register(Notice)
+class StoryAdmin(admin.ModelAdmin):
+	list_display = ['title', 'contents', 'date']
