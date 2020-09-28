8e0d46083a32ed30bf2f3f63d3629ec4d87e8749
KangJunewoo <brianjune.dev@gmail.com>
2020-09-28 12:57:01 +0900

    [FEAT] views.py 뼈대 완성

--- learningfair/applications/views.py
+++ learningfair/applications/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.core import serializers
-from django.http import JsonResponse
-from django.http import HttpResponse
+from django.http import JsonResponse, HttpResponse
+from datetime import date
 from .models import Project
 
 # json response 테스트 view
@@ -10,12 +10,12 @@ def test(req):
     project_list = serializers.serialize('json', projects)
     return HttpResponse(project_list, content_type="text/json")
 
-# /hotprojcets
+# GET /hotprojcets
 # 인기플젝 3개 조회
 def hotprojects(req):
     # TODO : db에서 좋아요 많은 플젝 3개 찾기
     data = {
-        '1':{
+        '1': {
             'title':'인기많은플젝',
             'campus':1,
             'lecture':1,
@@ -57,10 +57,9 @@ def hotprojects(req):
     }
     return JsonResponse(data)
 
-# /lectures/02/projects
+# GET /lectures/02/projects
 # 02분반 프로젝트 조회
-
-# TODO : 쿼리스트링 : page, sortBy=likes
+# query : page, sortBy
 def lectures(req, lecture_id):
     page = req.GET.get('page')
     sortBy = req.GET.get('sortBy')
@@ -71,12 +70,45 @@ def lectures(req, lecture_id):
     }
     return JsonResponse(data)
 
-# /projects
+# GET /projects?searchBy='머신러닝'
+# 프로젝트 전체에서 '머신러닝' 검색결과 조회
 def projects(req):
-    pass
+    searchBy = req.GET.get('searchBy')
+    data = {
+        'searchBy':searchBy
+    }
+    return JsonResponse(data)
 
+# GET /notices
+# 공지사항 조회
 def notices(req):
-    pass
+    data = {
+        '1':{
+            'title':'공지1',
+            'contents':'공지내용1',
+            'date':date(2020,1,1)
+        },
+        '2':{
+            'title':'공지1',
+            'contents':'공지내용1',
+            'date':date(2020,2,1)
+        },
+        '3':{
+            'title':'공지1',
+            'contents':'공지내용1',
+            'date':date(2020,3,1)
+        }
+    }
+    return JsonResponse(data)
 
-def project_likes(req):
-    pass
+# POST /projects/:project_id/likes
+# DELETE /projects/:project_id/likes
+def project_likes(req, project_id):
+    if req.method == 'POST':
+        # TODO : 좋아요 증가 처리
+        return JsonResponse({'status':f'{project_id} 좋아요 성공'})
+    elif req.method == 'DELETE':
+        # TODO : 좋아요 삭제 처리
+        return JsonResponse({'status':f'{project_id} 좋아요삭제 성공'})
+    else:
+        return JsonResponse({'status':'유효하지 않은 접근'})
