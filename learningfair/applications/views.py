ce5cd5abc5c6a143c8909faa3a71628b867b5eaa
KangJunewoo <brianjune.dev@gmail.com>
2020-09-28 11:04:09 +0900

    [FEAT] hotprojects 더미데이터 리턴 성공

--- learningfair/applications/views.py
+++ learningfair/applications/views.py
@@ -1,3 +1,61 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
+
+def hotprojects(req):
+    # db에서 찾기
+    data = {
+        '1':{
+            'title':'인기많은플젝',
+            'campus':1,
+            'lecture':1,
+            'groupNumber':1,
+            'groupName':'그룹1',
+            'members':['멤버1','멤버2','멤버3'],
+            'description':'그룹1 플젝입니다',
+            'pdf':'해당피디에프아이디',
+            'description':'프로젝트 설명설명',
+            'video':'비디오 아이디',
+            'likeCount':100
+        },
+        '2':{
+            'title':'두번째로인기많은플젝',
+            'campus':2,
+            'lecture':2,
+            'groupNumber':2,
+            'groupName':'그룹2',
+            'members':['멤버1','멤버2','멤버3'],
+            'description':'그룹2 플젝입니다',
+            'pdf':'해당피디에프아이디',
+            'description':'프로젝트 설명설명',
+            'video':'비디오 아이디',
+            'likeCount':10
+        },
+        '3':{
+            'title':'세번째로인기많은플젝',
+            'campus':3,
+            'lecture':3,
+            'groupNumber':3,
+            'groupName':'그룹3',
+            'members':['멤버1','멤버2','멤버3'],
+            'description':'그룹3 플젝입니다',
+            'pdf':'해당피디에프아이디',
+            'description':'프로젝트 설명설명',
+            'video':'비디오 아이디',
+            'likeCount':1
+        },
+    }
+    return JsonResponse(data)
+
+def classes(req):
+    pass
+
+def projects(req):
+    pass
+
+def notices(req):
+    pass
+
+def project_likes(req):
+    pass
