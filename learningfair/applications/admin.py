274164ea71e0b6c36c3b8ef20cfe6dd0d75b97b2
Heedong Kim <ruthetum@gmail.com>
2020-11-26 14:48:23 +0900

    [FEAT] Admin Action - csv 파일 다운

--- learningfair/applications/admin.py
+++ learningfair/applications/admin.py
@@ -1,10 +1,36 @@
 from django.contrib import admin
 from .models import User, Project, Like, Notice
+from datetime import date
+import csv
+from django.http import HttpResponse
+
+def download_csv(modeladmin, request, queryset):    
+	today = str(date.today())
+
+	# local writing and download
+	fE = open("{}(EUC-KR).csv".format(today), "w", encoding='EUC-KR')
+	fU = open("{}(UTF-8).csv".format(today), "w", encoding='UTF-8')
+	writerE = csv.writer(fE)
+	writerU = csv.writer(fU)
+	writerE.writerow(['date', 'major', 'studentId', 'name', 'comment'])
+	writerU.writerow(['date', 'major', 'studentId', 'name', 'comment'])
+	for q in queryset:
+		writerE.writerow([q.date, q.major, q.studentId, q.name, q.comment])
+		writerU.writerow([q.date, q.major, q.studentId, q.name, q.comment])
+	fE.close()
+	fU.close()
+	
+	# download by admin side
+	response = HttpResponse(open('{}(UTF-8).csv'.format(today), 'rt',encoding='UTF-8').read())
+	response['Content-Type']='text/csv'
+	response['Content-Disposition'] = 'attachment; filename={}(UTF-8).csv'.format(today)
+	return response
 
 # admin customizing
 @admin.register(User) # decorator 형태로 등록
 class StoryAdmin(admin.ModelAdmin):
 	list_display = ['date', 'major', 'studentId', 'name', 'comment']
+	actions = [download_csv]
 
 @admin.register(Project)
 class StoryAdmin(admin.ModelAdmin):
