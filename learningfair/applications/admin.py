979681bd13e59ee653d8ed4966f304da876f582a
Heedong Kim <ruthetum@gmail.com>
2020-12-02 05:18:22 +0900

    [FEAT] 엑셀 다운로드

--- learningfair/applications/admin.py
+++ learningfair/applications/admin.py
@@ -1,24 +1,44 @@
 from django.contrib import admin
 from .models import User, Project, Like, Notice
 from datetime import date
+import datetime
 import csv
 from django.http import HttpResponse
+import os
 
 def download_csv(modeladmin, request, queryset):    
 	today = str(date.today())
 
 	# local writing and download
-	fE = open("{}(EUC-KR).csv".format(today), "w", encoding='EUC-KR')
-	fU = open("{}(UTF-8).csv".format(today), "w", encoding='UTF-8')
-	writerE = csv.writer(fE)
-	writerU = csv.writer(fU)
-	writerE.writerow(['date', 'major', 'studentId', 'name', 'comment'])
-	writerU.writerow(['date', 'major', 'studentId', 'name', 'comment'])
-	for q in queryset:
-		writerE.writerow([q.date, q.major, q.studentId, q.name, q.comment])
-		writerU.writerow([q.date, q.major, q.studentId, q.name, q.comment])
-	fE.close()
-	fU.close()
+	if os.path.isfile("{}(EUC-KR).csv".format(today)):
+		fE = open("{}(EUC-KR).csv".format(today), "a", encoding='EUC-KR')
+		fU = open("{}(UTF-8).csv".format(today), "a", encoding='UTF-8')
+		now = str(datetime.datetime.now())
+		writerE = csv.writer(fE)
+		writerU = csv.writer(fU)
+		writerE.writerow(["{}".format(now)])
+		writerU.writerow(["{}".format(now)])
+		writerE.writerow(['date', 'major', 'studentId', 'name', 'comment'])
+		writerU.writerow(['date', 'major', 'studentId', 'name', 'comment'])
+		for q in queryset:
+			writerE.writerow([q.date, q.major, q.studentId, q.name, q.comment])
+			writerU.writerow([q.date, q.major, q.studentId, q.name, q.comment])
+		fE.close()
+		fU.close()
+	else:
+		fE = open("{}(EUC-KR).csv".format(today), "w", encoding='EUC-KR')
+		fU = open("{}(UTF-8).csv".format(today), "w", encoding='UTF-8')
+		writerE = csv.writer(fE)
+		writerU = csv.writer(fU)
+		writerE.writerow(["{}".format(now)])
+		writerU.writerow(["{}".format(now)])
+		writerE.writerow(['date', 'major', 'studentId', 'name', 'comment'])
+		writerU.writerow(['date', 'major', 'studentId', 'name', 'comment'])
+		for q in queryset:
+			writerE.writerow([q.date, q.major, q.studentId, q.name, q.comment])
+			writerU.writerow([q.date, q.major, q.studentId, q.name, q.comment])
+		fE.close()
+		fU.close()
 	
 	# download by admin side
 	response = HttpResponse(open('{}(UTF-8).csv'.format(today), 'rt',encoding='UTF-8').read())
